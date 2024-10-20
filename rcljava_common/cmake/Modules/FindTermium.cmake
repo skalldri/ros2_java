510e3479d11b4ae306fdd101f7d367b7cba0d66f
Stuart Alldritt <s.k.alldritt@gmail.com>
2024-10-19 19:22:02 -0700

    Use OpenJDK 11 Termium

--- rcljava_common/cmake/Modules/FindTermium.cmake
+++ rcljava_common/cmake/Modules/FindTermium.cmake
@@ -3,14 +3,12 @@ include(FetchContent)
 
 FetchContent_Declare(
     Termium
-    URL https://api.adoptium.net/v3/binary/latest/17/ga/linux/x64/jdk/hotspot/normal/eclipse
+    URL https://api.adoptium.net/v3/binary/latest/11/ga/linux/x64/jdk/hotspot/normal/eclipse
     DOWNLOAD_EXTRACT_TIMESTAMP false
 )
 
 FetchContent_GetProperties(Termium)
 
 FetchContent_MakeAvailable(Termium)
-
-message(WARNING "Hello my name is ${termium_SOURCE_DIR}")
 
 set(Termium_FOUND true)
