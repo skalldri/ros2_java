6d74b97e3a6125edcfc7c34264b19dab44059355
Stuart Alldritt <s.k.alldritt@gmail.com>
2024-10-20 00:56:20 -0700

    Fix NOT ANDROID compile

--- termium_java/termium_java-extra.cmake
+++ termium_java/termium_java-extra.cmake
@@ -1,4 +1,2 @@
 file(REAL_PATH "${termium_java_DIR}/../../../java_home" RESOLVED_TERMIUM_JAVA_HOME)
 set(JAVA_HOME "${RESOLVED_TERMIUM_JAVA_HOME}" CACHE INTERNAL "JAVA_HOME")
-
-message(WARNING "Forcing JAVA_HOME to ${JAVA_HOME} for Termium Java Install")
